1700055341c943aae115bc3f47125aaebe5dc983
KarlosJRodriguez <deathkarlos07@gmail.com>
2021-03-22 20:01:28 -0600

    cambios realizados al archivo

--- tipoexamen.py
+++ tipoexamen.py
@@ -1,9 +1,15 @@
 op = int(input("Ingrese un numero: "))
 if op == 1:
-	print("Ganste");
+	print("Ganaste");
 elif op == 2:
 	print("Empataste");
 elif op == 3:
 	print("Perdiste");
 else:
 	print("Opcion no valida");
+
+tupla=(9,8,7,6,5,4,3,2,1)
+lista=[9,8,7,6,5,4,3,2,1]
+
+print(tupla);
+print(lista);
